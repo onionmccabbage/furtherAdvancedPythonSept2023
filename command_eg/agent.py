5e2007f822a71a2fb42b4af8b25162b3966b5e4a
onionmccabbage <onionmccabbage@live.com>
2023-09-04 12:30:24 +0100

    lunch day 1

--- command_eg/agent.py
+++ command_eg/agent.py
@@ -4,6 +4,7 @@ class Agent():
         self.__orderQueue = [] # start with an empty array
     def placeOrder(self, order):
         self.__orderQueue.append(order)
+        # the queue might be asynchronous, with latency, so things might queue up
         order.execute()
 
 if __name__ == '__main__':
