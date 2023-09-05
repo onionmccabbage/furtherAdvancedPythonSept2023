68d7b9458eac1871f5617600cc83de111164c282
onionmccabbage <onionmccabbage@live.com>
2023-09-05 16:25:24 +0100

    gevent and async

--- using_threads/legacy/using_coroutines.py
+++ using_threads/legacy/using_coroutines.py
@@ -1,1 +1,22 @@
-#mordern python
+# modern python lets us manage asynchronous code using 'async'
+# previously we would use coroutines for this
+
+import asyncio
+
+@asyncio.coroutine
+def cube(n):
+    '''this needs to be asynchronous (maybe it takes a loooong time)'''
+    return n*n*n
+
+# now there is no need to import anything, async is built in to python
+async def square(n):
+    '''might be a long running function'''
+    await( asyncio.sleep(2) ) # we can await any async function
+    return n*n
+
+# NB both functions will be threaded (due to async)
+if __name__ == '__main__':
+    s = asyncio.run(cube(3))
+    print(s) #27
+    d = asyncio.run(square(2))
+    print(d) # 4
