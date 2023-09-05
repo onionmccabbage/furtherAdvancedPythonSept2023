9050b5c54d68b5997cdbec047b326a09c08862fa
onionmccabbage <onionmccabbage@live.com>
2023-09-05 10:59:46 +0100

    sample class and function threads

--- using_threads/thread_types.py
+++ using_threads/thread_types.py
@@ -1,10 +1,12 @@
 # we can run many threads on one processor
 # threads may be classes or functions
 
+from collections.abc import Callable, Iterable, Mapping
 from threading import Thread # Thread is a thread-access-object (not an actual Thread)
 
 import time
 import random
+from typing import Any
 
 def someFn(n):
     '''this function will simply sleep for a short time'''
@@ -12,28 +14,39 @@ def someFn(n):
         time.sleep(random.random()*0.1 ) # sleep for up to a tenth of a second
         print(n)
 
-
-
-
-
+class SomeClass(Thread):
+    def __init__(self, n):
+        Thread.__init__(self) # call the __init__ of the parent class
+        self.n = n
+    # the Thread class has a 'run' method which we can override
+    def run(self):
+        for i in range(0, 50):
+            time.sleep(random.random()*0.1 ) # sleep for up to a tenth of a second
+            print(self.n)
 if __name__ == '__main__':
     t1 = Thread(target=someFn, args=('thread 1',)) # Arguments are provided in a tuple
     t2 = Thread(target=someFn, args=('thread 2',)) # Arguments are provided in a tuple
     t3 = Thread(target=someFn, args=('thread 3',)) # Arguments are provided in a tuple
     t4 = Thread(target=someFn, args=('thread 4',)) # Arguments are provided in a tuple
+    
+    tA = SomeClass('thread A')
+    tB = SomeClass('thread B')
+    tC = SomeClass('thread C')
+    tD = SomeClass('thread D')
+    
     # we can measure time
     start = time.time()
 
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
+    tA.start()
+    tB.start()
+    tC.start()
+    tD.start()
 
     # we can make our main thread pause until the other threads are complete
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
+    tA.join()
+    tB.join()
+    tC.join()
+    tD.join()
 
     end = time.time()
     # careful - the main thread just carries on despite other threads running
