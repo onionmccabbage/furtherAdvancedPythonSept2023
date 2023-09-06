e6d7bd26278b28215b5119dfc2eddd9f5308b2a9
onionmccabbage <onionmccabbage@live.com>
2023-09-06 10:36:09 +0100

    completed class method lock example

--- using_threads/my_thread_decorator.py
+++ using_threads/my_thread_decorator.py
@@ -23,10 +23,25 @@ def lock_a_method(meth):
     locked_method.__is_locked = True
     return locked_method
 
+def make_thread_safe(cls, meth_l, lock_factory):
+    '''iterate over all the methods in meth_l making each thread safe'''
+    init = cls.__init__ # take a copy of the current __init__ method
+    def new_init(self, *args, **kwargs):
+        init(self, *args, **kwargs)
+        self.__lock = lock_factory # typically a 'Lock'
+    cls.__init__ = new_init # replace the original __init__ method
+    for meth in meth_l:
+        old_meth = getattr(cls, meth)
+        new_meth = lock_a_method(old_meth)
+        setattr(cls, meth, new_meth)
+    return cls
 
-
+# here is a function to be used as a decorator to make selected methods of the class thread safe
+def lock_a_class(meth_list, lock_factory): # lock_factory is any 'Lock' type
+    return lambda cls: make_thread_safe(cls, meth_list, lock_factory)
 
 # here is a class - we will apply our decorator to lock some methods of this class
+@lock_a_class(['add', 'remove'], Lock)
 class MyClass(set): # here we extend the set type
     def __init__(self, new_set):
         set.__init__(self, new_set) # call the initializer of the 'set' type
@@ -41,3 +56,6 @@ if __name__ == '__main__':
     # see if 'someMethod' is locked
     s.someMethod() # call the actual method
     print(s.someMethod.__is_locked) # False or error ( it doesn't exist)
+    # try out the lock_a_class
+    s.add('apples')
+    s.remove(2)
