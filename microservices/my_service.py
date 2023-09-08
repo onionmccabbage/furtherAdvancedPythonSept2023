08507aa606d48834b28e9c16f94e697a0e561919
onionmccabbage <onionmccabbage@live.com>
2023-09-08 11:31:13 +0100

    weather microservice

--- microservices/my_service.py
+++ microservices/my_service.py
@@ -24,9 +24,17 @@ def myServer():
         if buf == b'quit':
             break # stop this microservice 
         else:
+            url_template = f'http://api.openweathermap.org/data/2.5/weather?q={buf_str}&units=metric&APPID=48f2d5e18b0d2bc50519b58cce6409f1'
+            print(f'server is trying {url_template}')
+            response = requests.get(url_template)
+            data = response.json()
+            print(f'Server received {data}')
+            # read one part of the weahter report
+            descr = data['weather'][0]['description']
             # send a response to the client
-            response = buf.upper()
-            client.send(response)
+            # response = buf.upper()
+            # client.send(response)
+            client.send(descr.encode())
 
 if __name__ == '__main__':
     myServer()
